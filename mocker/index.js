942cbd413347915033dd9d5a7d0d3dc8ef36e90b
Jacek Raczykowski <jacek@raczykowski.pl>
2020-03-15 21:33:32 +0100

    cleanup

--- mocker/index.js
+++ mocker/index.js
@@ -1,6 +1,5 @@
 const delay = require('mocker-api/utils/delay')
 
-// 是否禁用代理
 const noProxy = process.env.NO_PROXY === 'true'
 
 const proxy = {
@@ -13,10 +12,9 @@ const proxy = {
     console.log('app --->', req.params)
     return res.json({
       id: req.params.id,
-      title: 'App title ' + req.params.id,
+      title: 'MockerApi App title ' + req.params.id,
       sex: 6,
     })
   },
 }
 module.exports = noProxy ? {} : delay(proxy, 1000)
-// module.exports = proxy;
